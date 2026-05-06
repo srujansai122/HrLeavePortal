f3dcc639aa427619c103e64fe3256ef442df0763
srujansai122 <saisrujangundeti@gmail.com>
2026-05-06 23:02:53 +0530

    Implemented CRUD features for Leave Types

---
+++ LeaveManagement/Data/LeaveType.cs
@@ -0,0 +1,13 @@
+using System;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace LeaveManagement.Data;
+
+public class LeaveType
+{
+    public int Id { get; set; }
+
+    [Column(TypeName = "nvarchar(100)")]
+    public string Name { get; set; }
+    public int NumberOfDays { get; set; }
+}
